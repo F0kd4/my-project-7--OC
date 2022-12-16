30e2d2e0eb5818572b9afafd8b9952c9abf3e561
4rty <kingofpunk_edc@hotmail.fr>
2022-12-17 00:04:23 +0100

    page home 1.0 + améliorations générales

--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -3,9 +3,9 @@ import React from "react";
 const Thumb = ({ cover, title }) => {
   return (
     <div className="thumb">
-      <div className="logement__card">
-        <img src={cover} alt="couverture logement" />
-        <h2 className="logement__card__title">{title}</h2>
+      <div className="thumb__card">
+        <img src={cover} alt="couverture logement" className="thumb__card__img" />
+        <h2 className="thumb__card__title">{title}</h2>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 import Homebanner from "../components/Homebanner";
 import Thumb from "../components/Thumb";
 
@@ -13,18 +12,23 @@ const Home = () => {
   return (
     <div className="home_page">
       <section className="home__page__section1">
-        <Homebanner cover={image}/>
+        <Homebanner cover={image} />
       </section>
       <section className="home__page__section2">
-        {Logements.map((logement) => {
-          return (
-            <article key={logement.id}>
-              <Link to={`/logement/${logement.id}`}>
-                <Thumb cover={logement.cover} title={logement.title} />
-              </Link>
-            </article>
-          );
-        })}
+        <div className="display">
+          {Logements.map((logement) => {
+            return (
+              <article className="home__page__section2__card" key={logement.id}>
+                <Link
+                  to={`/logement/${logement.id}`}
+                  className="home__page__section2__card__link"
+                >
+                  <Thumb cover={logement.cover} title={logement.title} />
+                </Link>
+              </article>
+            );
+          })}
+        </div>
       </section>
     </div>
   );
